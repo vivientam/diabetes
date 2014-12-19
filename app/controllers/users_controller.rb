701377c56423fe75de45606e645c3d51250c36ef
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2014-12-19 10:55:53 +0800

    div structure done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,39 @@
 class UsersController < ApplicationController
 
-
-  def index
-  end
-
-  def name
-  end
-
-  def title
-  end
-
-  def email
-
-  end
-
-  def hospital
-  end
-
-  def dateOfBirth
-  end
+before_action :authenticate_user!, only: [:new, :create]
   
+  def index
+    
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = current_user.users.new(user_params)
+    
+    if user.save
+      redirect_to users_path
+      else
+      # if the record doesnt save because it didnt pass the validations
+      flash[:message] = user.errors.messages
+      
+      redirect_to :back
+    end
+  end     
+  
+  def show
+    
+    @user = User.find(params[:id])
+    @comment = Comment.new
+  end
+
+  # this is only for internal use.
+  private
+    def user_params
+      params.require(:user).
+        permit(:title, :age)
+    end
 end
